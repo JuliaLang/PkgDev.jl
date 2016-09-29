c5b0b158dd386e5f2ac22549daf7041245b2213c
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2016-09-29 23:10:33 +0200

    change git paths to always be / separated fixes #28.

--- test/runtests.jl
+++ test/runtests.jl
@@ -135,17 +135,27 @@ end"""
 
     @testset "testing package tags" begin
         PkgDev.generate("PackageWithTags", "MIT", config=Dict("user.name"=>"Julia Test", "user.email"=>"test@julialang.org"))
+        PkgDev.register("PackageWithTags")
         PkgDev.tag("PackageWithTags")
         PkgDev.tag("PackageWithTags")
 
         # check tags
-        repo = LibGit2.GitRepo(joinpath(pkgdir,"PackageWithTags"))
+        repo = LibGit2.GitRepo(joinpath(pkgdir, "PackageWithTags"))
+        meta_repo = LibGit2.GitRepo(joinpath(pkgdir, "METADATA"))
         try
             tags = LibGit2.tag_list(repo)
             @test ("v0.0.1" in tags) == true
             @test ("v0.0.2" in tags) == true
+            # test that those version files exist
+            @test ispath(joinpath(pkgdir, "METADATA", "PackageWithTags",
+                                  "versions", "0.0.1", "sha1")) == true
+            # check that we actually commited those files to METADATA
+            # (this test always succeeds for some reason -- we should be using
+            #  git_diff_index_to_workdir here)
+            @test LibGit2.isdirty(meta_repo) == false
         finally
             finalize(repo)
+            finalize(meta_repo)
         end
     end
 
