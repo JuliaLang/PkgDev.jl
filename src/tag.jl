d1cca0461ce8d4cb602f6ea6d78848d5a620d7b5
David Anthoff <anthoff@berkeley.edu>
2019-09-13 16:01:10 -0700

    Clean up resources better

--- src/tag.jl
+++ src/tag.jl
@@ -53,107 +53,116 @@ function tag(package_name::AbstractString, version::Union{Symbol,VersionNumber,N
 
     pkg_repo = GitRepo(pkg_path)
 
-    LibGit2.isdirty(pkg_repo) && error("The repo for the package cannot be dirty.")
+    try
 
-    pkg_remote = LibGit2.lookup_remote(pkg_repo, "origin")
+        LibGit2.isdirty(pkg_repo) && error("The repo for the package cannot be dirty.")
 
-    pkg_remote===nothing && error("The package must have a remote called origin.")
+        pkg_remote = LibGit2.lookup_remote(pkg_repo, "origin")
 
-    pkg_url = LibGit2.url(pkg_remote)
+        pkg_remote===nothing && error("The package must have a remote called origin.")
 
-    pkg_project_toml_path = isfile(joinpath(pkg_path, "JuliaProject.toml")) ? joinpath(pkg_path, "JuliaProject.toml") : isfile(joinpath(pkg_path, "Project.toml")) ? joinpath(pkg_path, "Project.toml") : error("Couldn't find Project.toml.")
+        pkg_url = LibGit2.url(pkg_remote)
 
-    pkg_toml_content = Pkg.TOML.parsefile(pkg_project_toml_path)
+        pkg_project_toml_path = isfile(joinpath(pkg_path, "JuliaProject.toml")) ? joinpath(pkg_path, "JuliaProject.toml") : isfile(joinpath(pkg_path, "Project.toml")) ? joinpath(pkg_path, "Project.toml") : error("Couldn't find Project.toml.")
 
-    haskey(pkg_toml_content, "version") || error("Project.toml must have a version field.")
+        pkg_toml_content = Pkg.TOML.parsefile(pkg_project_toml_path)
 
-    current_version_in_pkg = VersionNumber(pkg_toml_content["version"])
+        haskey(pkg_toml_content, "version") || error("Project.toml must have a version field.")
 
-    version_to_be_tagged = if version===nothing
-        current_version_in_pkg.prerelease!=("DEV",) && current_version_in_pkg.build!=() && error("Version in Project.toml must have format of x.y.z-DEV.")
-        VersionNumber(current_version_in_pkg.major, current_version_in_pkg.minor, current_version_in_pkg.patch)
-    elseif version isa VersionNumber
-        version
-    elseif version==:major
-        VersionNumber(current_version_in_pkg.major+1, 0, 0)
-    elseif version==:minor
-        VersionNumber(current_version_in_pkg.major, current_version_in_pkg.minor+1, 0)
-    elseif version==:patch && current_version_in_pkg.prerelease==("DEV",)
-        VersionNumber(current_version_in_pkg.major, current_version_in_pkg.minor, current_version_in_pkg.patch)
-    elseif version==:patch
-        VersionNumber(current_version_in_pkg.major, current_version_in_pkg.minor, current_version_in_pkg.patch+1)
-    else
-        error("Invalid argument for version, must be nothing, a VersionNumber, :major, :minor or :patch.")
-    end
+        current_version_in_pkg = VersionNumber(pkg_toml_content["version"])
 
-    next_version = VersionNumber(version_to_be_tagged.major, version_to_be_tagged.minor, version_to_be_tagged.patch+1, ("DEV",))
-
-    # TODO Check whether that version already exists, and if so, error.
-
-    name_of_release_branch = "release-$version_to_be_tagged"
-
-    LibGit2.lookup_branch(pkg_repo, name_of_release_branch)===nothing || error("A branch named $name_of_release_branch already exists in the package.")
-
-    name_of_old_branch_in_pkg = LibGit2.headname(pkg_repo)
-
-    LibGit2.branch!(pkg_repo, name_of_release_branch, force=true)
-
-    # Now update the version field in the Project.toml
-
-    pkg_toml_content["version"] = version_to_be_tagged
-
-    open(pkg_project_toml_path, "w") do f
-        Pkg.TOML.print(f, pkg_toml_content)
-    end
-
-    project_as_it_should_be_tagged = Pkg.Types.read_project(pkg_project_toml_path)
-
-    LibGit2.add!(pkg_repo, splitdir(pkg_project_toml_path)[2])
-    hash_of_commit_to_be_tagged = LibGit2.commit(pkg_repo, "Set version to v$version_to_be_tagged")
-
-    tree_hash_of_commit_to_be_tagged = LibGit2.GitHash(LibGit2.peel(LibGit2.GitTree, LibGit2.GitCommit(pkg_repo, hash_of_commit_to_be_tagged)))
-
-    # Now update the version field in the Project.toml
-
-    pkg_toml_content = Pkg.TOML.parsefile(pkg_project_toml_path)
-
-    pkg_toml_content["version"] = next_version
-
-    open(pkg_project_toml_path, "w") do f
-        Pkg.TOML.print(f, pkg_toml_content)
-    end
-
-    LibGit2.add!(pkg_repo, splitdir(pkg_project_toml_path)[2])
-    LibGit2.commit(pkg_repo, "Set version to v$next_version")
-
-    # TODO We get an error here with private repos, but then everything continues
-    LibGit2.push(pkg_repo, refspecs=["refs/heads/$name_of_release_branch"])
-    
-    LibGit2.branch!(pkg_repo, name_of_old_branch_in_pkg)
-
-    LibGit2.delete_branch(LibGit2.lookup_branch(pkg_repo, name_of_release_branch))
-
-    pkg_owner_repo_name = splitext(URI(pkg_url).path)[1][2:end]
-
-    gh_pkg_repo = GitHub.repo(pkg_owner_repo_name, auth=myauth)
-
-    GitHub.create_pull_request(gh_pkg_repo, auth=myauth, params=Dict(:title=>"New version: v$version_to_be_tagged", :head=>name_of_release_branch, :base=>name_of_old_branch_in_pkg, :body=>""))
-
-    mktempdir() do tmp_path
-        cd(tmp_path) do
-            folder_for_registry = nothing
-            regbranch = if private_reg_url===nothing
-                folder_for_registry = joinpath(tmp_path, "registries", "23338594-aafe-5451-b93e-139f81909106")
-                Registrator.RegEdit.register(pkg_url, project_as_it_should_be_tagged, string(tree_hash_of_commit_to_be_tagged); registry=general_reg_url, push=false)
-            else
-                folder_for_registry = joinpath(tmp_path, "registries", string(private_reg_uuid))
-                Registrator.RegEdit.register(pkg_url, project_as_it_should_be_tagged, string(tree_hash_of_commit_to_be_tagged); registry=private_reg_url, registry_deps=[general_reg_url], push=false)
-            end
-
-            registry_repo = GitRepo(folder_for_registry)
-            LibGit2.push(registry_repo, remoteurl=registry_fork_url, refspecs=["refs/heads/$(regbranch.branch)"])
-
-            GitHub.create_pull_request(gh_registry_repo, auth=myauth, params=Dict(:title=>"New version: $package_name v$version_to_be_tagged", :head=>"$github_username:$(regbranch.branch)", :base=>"master", :body=>""))
+        version_to_be_tagged = if version===nothing
+            current_version_in_pkg.prerelease!=("DEV",) && current_version_in_pkg.build!=() && error("Version in Project.toml must have format of x.y.z-DEV.")
+            VersionNumber(current_version_in_pkg.major, current_version_in_pkg.minor, current_version_in_pkg.patch)
+        elseif version isa VersionNumber
+            version
+        elseif version==:major
+            VersionNumber(current_version_in_pkg.major+1, 0, 0)
+        elseif version==:minor
+            VersionNumber(current_version_in_pkg.major, current_version_in_pkg.minor+1, 0)
+        elseif version==:patch && current_version_in_pkg.prerelease==("DEV",)
+            VersionNumber(current_version_in_pkg.major, current_version_in_pkg.minor, current_version_in_pkg.patch)
+        elseif version==:patch
+            VersionNumber(current_version_in_pkg.major, current_version_in_pkg.minor, current_version_in_pkg.patch+1)
+        else
+            error("Invalid argument for version, must be nothing, a VersionNumber, :major, :minor or :patch.")
         end
+
+        next_version = VersionNumber(version_to_be_tagged.major, version_to_be_tagged.minor, version_to_be_tagged.patch+1, ("DEV",))
+
+        # TODO Check whether that version already exists, and if so, error.
+
+        name_of_release_branch = "release-$version_to_be_tagged"
+
+        LibGit2.lookup_branch(pkg_repo, name_of_release_branch)===nothing || error("A branch named $name_of_release_branch already exists in the package.")
+
+        name_of_old_branch_in_pkg = LibGit2.headname(pkg_repo)
+
+        LibGit2.branch!(pkg_repo, name_of_release_branch, force=true)
+
+        # Now update the version field in the Project.toml
+
+        pkg_toml_content["version"] = version_to_be_tagged
+
+        open(pkg_project_toml_path, "w") do f
+            Pkg.TOML.print(f, pkg_toml_content)
+        end
+
+        project_as_it_should_be_tagged = Pkg.Types.read_project(pkg_project_toml_path)
+
+        LibGit2.add!(pkg_repo, splitdir(pkg_project_toml_path)[2])
+        hash_of_commit_to_be_tagged = LibGit2.commit(pkg_repo, "Set version to v$version_to_be_tagged")
+
+        tree_hash_of_commit_to_be_tagged = LibGit2.GitHash(LibGit2.peel(LibGit2.GitTree, LibGit2.GitCommit(pkg_repo, hash_of_commit_to_be_tagged)))
+
+        # Now update the version field in the Project.toml
+
+        pkg_toml_content = Pkg.TOML.parsefile(pkg_project_toml_path)
+
+        pkg_toml_content["version"] = next_version
+
+        open(pkg_project_toml_path, "w") do f
+            Pkg.TOML.print(f, pkg_toml_content)
+        end
+
+        LibGit2.add!(pkg_repo, splitdir(pkg_project_toml_path)[2])
+        LibGit2.commit(pkg_repo, "Set version to v$next_version")
+
+        # TODO We get an error here with private repos, but then everything continues
+        LibGit2.push(pkg_repo, refspecs=["refs/heads/$name_of_release_branch"])
+        
+        LibGit2.branch!(pkg_repo, name_of_old_branch_in_pkg)
+
+        LibGit2.delete_branch(LibGit2.lookup_branch(pkg_repo, name_of_release_branch))
+
+        pkg_owner_repo_name = splitext(URI(pkg_url).path)[1][2:end]
+
+        gh_pkg_repo = GitHub.repo(pkg_owner_repo_name, auth=myauth)
+
+        GitHub.create_pull_request(gh_pkg_repo, auth=myauth, params=Dict(:title=>"New version: v$version_to_be_tagged", :head=>name_of_release_branch, :base=>name_of_old_branch_in_pkg, :body=>""))
+
+        mktempdir() do tmp_path
+            cd(tmp_path) do
+                folder_for_registry = nothing
+                regbranch = if private_reg_url===nothing
+                    folder_for_registry = joinpath(tmp_path, "registries", "23338594-aafe-5451-b93e-139f81909106")
+                    Registrator.RegEdit.register(pkg_url, project_as_it_should_be_tagged, string(tree_hash_of_commit_to_be_tagged); registry=general_reg_url, push=false)
+                else
+                    folder_for_registry = joinpath(tmp_path, "registries", string(private_reg_uuid))
+                    Registrator.RegEdit.register(pkg_url, project_as_it_should_be_tagged, string(tree_hash_of_commit_to_be_tagged); registry=private_reg_url, registry_deps=[general_reg_url], push=false)
+                end
+
+                registry_repo = GitRepo(folder_for_registry)
+                try
+                    LibGit2.push(registry_repo, remoteurl=registry_fork_url, refspecs=["refs/heads/$(regbranch.branch)"])
+                finally
+                    close(registry_repo)
+                end 
+                
+                GitHub.create_pull_request(gh_registry_repo, auth=myauth, params=Dict(:title=>"New version: $package_name v$version_to_be_tagged", :head=>"$github_username:$(regbranch.branch)", :base=>"master", :body=>""))
+            end
+        end
+    finally
+        close(pkg_repo)
     end
 end
