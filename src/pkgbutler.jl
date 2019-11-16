56d1781fc37ebe8bd76ac01b29f4556b8660fada
David Anthoff <anthoff@berkeley.edu>
2019-11-16 13:33:17 -0800

    Add channel option to enable_pkgbutler

--- src/pkgbutler.jl
+++ src/pkgbutler.jl
@@ -1,11 +1,11 @@
-function enable_pkgbutler(package_name::AbstractString)
+function enable_pkgbutler(package_name::AbstractString, channel=:auto)
     ctx = Pkg.Types.Context()
     haskey(ctx.env.project.deps, package_name) || error("Unkonwn package $package_name.")
     pkg_uuid = ctx.env.project.deps[package_name]
     pkg_path = ctx.env.manifest[pkg_uuid].path
     pkg_path===nothing && error("Package must be deved to enable the Julia Package Butler.")
 
-    PkgButlerEngine.update_pkg(pkg_path)
+    PkgButlerEngine.update_pkg(pkg_path, channel=channel)
 end
 
 function configure_deploykey()
